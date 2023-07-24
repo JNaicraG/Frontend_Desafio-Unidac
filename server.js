6a0011160779e1d4f087428eb92fb03a7f7458f7
GarciaNJ <nicolas.jg.njg@gmail.com>
2023-07-24 04:46:53 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,32 +3,18 @@
 //https://stackoverflow.com/questions/18864677/what-is-process-env-port-in-node-js
 
 
-const bodyParser = require('body-parser');
-const port = process.env.PORT || 3000; 
-const express = require('express'); 
-const app = express();                              
+const express = require('express');
+const http = require('http');
+const path = require('path');
 
-const allowCors = (req,res, next) => {
-    res.header("Access-Control-Allow-Origin", "*"); // colocar os dominios permitidos | ex: 127.0.0.1:3000
-    
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials, X-Access-Token, X-Key");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS, PATCH");
-    
-    res.header("Access-Control-Allow-Credentials", "true");
+const app = express();
 
-    next();
-}
+const port = process.env.PORT || 3001;
 
+app.use(express.static(__dirname + '/dist/desafio_unidac'));
 
-app.use(allowCors);
-app.use(bodyParser.json( { limit: "10mb"}));
-app.use(bodyParser.urlencoded({ extended : true }));
-app.use(express.static(__dirname, + "/dist/desafio_unidac"));
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
 
-app.get('*',(req,res) => {
-    res.sendFile(__dirname + "/dist/desafio_unidac/index.html");
-});
+const server = http.createServer(app);
 
-app.listen(port, async() => {
-    console.log(`API Gateway rodando na porta ${port}`);
-});
+server.listen(port, () => console.log(`App running on: http://localhost:${port}`));
